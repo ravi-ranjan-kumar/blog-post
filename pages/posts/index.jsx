e15d9b956c573a04dfea57e7a6c76cc5051330ac
Ravi Ranjan Kumar <ravi.ranjan@ucodesoft.com>
2023-01-10 16:12:02 +0530

    created all post page

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Layout from '../../components/Layout'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Layout>
+      <main>
+        
+      </main>
+    </Layout>
   )
 }
 
